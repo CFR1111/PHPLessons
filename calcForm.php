d764f642567a0000a21e24073a5fdb9a2451ff25
CFR1111 <58046762+CFR1111@users.noreply.github.com>
2019-11-21 11:27:26 -0800

    Create calcForm.php

---
+++ calcForm.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<form action="runCalc.php" method="get">
+			Num1 <input type="text" name="num1"><br>
+			Num2 <input type="text" name="num2"><br>
+			<select name="type">
+				<option>Add</option>
+				<option>Subtract</option>
+			</select>
+			<input type="submit" value="Calculate">
+		</form>
+</html>
