382f1ea5d39cce10ba2a5c8a327c0c837a4d9aaa
CFR1111 <58046762+CFR1111@users.noreply.github.com>
2019-11-21 11:28:03 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<style>
+			body{
+				margin:0px;
+			}
+			#bannerDiv{
+				background-color:grey;
+				height:100px;
+			}
+			.links{
+				color:maroon;
+				font-size:50px
+
+			}
+			#testPg{
+				margin-right:10px;
+			}
+			#formPg{
+				margin-right:10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="bannerDiv">
+			<a id="testPg" class="links" href="test.php">Test Page</a>
+			<a id="formPg" class="links" href="form.php">Form Page</a>
+			<a class="links" href="calcForm.php">Calculator Page</a>
+		</div>
+	</body>
+</html>
