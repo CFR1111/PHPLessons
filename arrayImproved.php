9ed94374c7cbc8d9781615fd1b3a8ec400965799
CFR1111 <58046762+CFR1111@users.noreply.github.com>
2019-11-22 11:04:37 -0800

    Create arrayImproved.php

---
+++ arrayImproved.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<style>
+			#peeps{
+				color:red;
+			}
+			.list{
+				color:maroon;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			$myFirstVar = "Hello World";
+			echo $myFirstVar;
+			echo "<h1 id='peeps'>".$myFirstVar."</h1>";
+			
+			$testArray = array('test1','test2','test3','test4','test5');
+			echo "<ol>";
+			foreach($testArray as $test){
+				echo "<li class='list'>".$test."</li>";
+			}
+			echo "</ol>";
+		?>
+	</body>
+</html>
